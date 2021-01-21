d4ef8d0a6e0a0b8f595a3c0c5ef327539b183797
e.ivanov <e.ivanov@PT0007.local>
2021-01-21 14:19:31 +0300

    Update replay.c

--- Manchester encoder-decoder.cydsn/replay.c
+++ Manchester encoder-decoder.cydsn/replay.c
@@ -26,40 +26,22 @@ int replay(void){
     return usb_context.need_to_send = 1;
 }
 int insert_header(char* pchar){
-    return sprintf(pchar,"$D");
+    return sprintf(pchar,"$DSKFE");
 }
-
-int long2five(char* pchar, uint32 L){
-    int cnt=0;
-    char* initial_pointer = pchar;
-    pchar +=sprintf(pchar,",");
-    char byte;
-    for(;cnt<5;cnt++){
-       byte = (L&0x7f)|0x80;
-       L>>=7;
-       pchar +=sprintf(pchar,"%c",byte);
-    }
-    return pchar - initial_pointer;
-}
-
 int insert_frame(char* pchar, frame_t frame){
     char* initial_pointer = pchar;
     for (int i=0;i<WORDS_QUANTITY;i++){
-        pchar += long2five(pchar, frame.items[i]);
+        pchar += sprintf(pchar,",%08lx",frame.items[i]);
     }
     return pchar - initial_pointer;
 }
-uint32 W_data=((((('a'<<8)+'t')<<8)+'a')<<8)+'d';
-uint32 W_smpl=((((('l'<<8)+'p')<<8)+'m')<<8)+'s';
-
-
 
 int insert_label(char* pchar){
     char* initial_pointer = pchar;
-    pchar += long2five(pchar, W_data);//sprintf(pchar,",%02x%02x%02x%02x",'a','t','a','d');
-    pchar += long2five(pchar, W_smpl);//sprintf(pchar,",%02x%02x%02x%02x",'l','p','m','s');
-    pchar += long2five(pchar, seconds);
-    pchar += long2five(pchar, mseconds);
+    pchar += sprintf(pchar,",%02x%02x%02x%02x",'d','a','t','a');
+    pchar += sprintf(pchar,",%02x%02x%02x%02x",'s','m','p','l');
+    pchar += sprintf(pchar,",%08d",seconds);
+    pchar += sprintf(pchar,",%08d",mseconds);
     return pchar - initial_pointer;
 }
 
@@ -68,7 +50,7 @@ int insert_crc8(char* pchar, char* pfirst){
     while (pfirst<pchar){
         crc8 ^= *(pfirst++);
     }
-    return sprintf(pchar,"*aa\r");//sprintf(pchar,"*%02x\n\r",crc8);
+    return sprintf(pchar,"*%02x\n\r",crc8);
 }
 
 int ms_marker(void){
